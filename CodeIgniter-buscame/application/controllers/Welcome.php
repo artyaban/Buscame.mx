9b152556bdde79298718a81c9c87f0b943dc7d12
Rafael Arturo Aban Gomez <artyaban@gmail.com>
2016-03-17 22:49:18 -0600

    masificación head footer

--- CodeIgniter-buscame/application/controllers/Welcome.php
+++ CodeIgniter-buscame/application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('main');
+		$this->load->view('footer');
+
 	}
 }
